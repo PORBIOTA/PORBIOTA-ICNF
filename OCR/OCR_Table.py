26bd9c7f579bb845605e428b779909931d49e3d1
CodenameGreyFox <63403237+CodenameGreyFox@users.noreply.github.com>
2021-09-09 16:39:49 +0100

    Update OCR_Table.py
    Added psm description.

--- OCR/OCR_Table.py
+++ OCR/OCR_Table.py
@@ -10,8 +10,25 @@ except ImportError:
 import pytesseract
 
 #Parameters
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+# --psm
+#0    Orientation and script detection (OSD) only.
+#1    Automatic page segmentation with OSD.
+#2    Automatic page segmentation, but no OSD, or OCR.
+#3    Fully automatic page segmentation, but no OSD. (Default)
+#4    Assume a single column of text of variable sizes.
+#5    Assume a single uniform block of vertically aligned text.
+#6    Assume a single uniform block of text.
+#7    Treat the image as a single text line.
+#8    Treat the image as a single word.
+#9    Treat the image as a single word in a circle.
+#10    Treat the image as a single character.
+#11    Sparse text. Find as much text as possible in no particular order.
+#12    Sparse text with OSD.
+#13    Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+
 tesseractConfig = '--psm 4 --oem 1 -l eng'
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 outputFile=r'C:/output.csv'
 file=r'G:/Porbiota/OCR_entomologia-20210908T161645Z-001/OCR_entomologia/177_1997/max_treatment_Page_3.jpg'
 
