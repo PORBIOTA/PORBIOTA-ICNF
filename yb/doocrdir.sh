07b87b18983b818ab1e600ceed948180f3ee27e9
Yuri <87668108+y1bpb@users.noreply.github.com>
2021-10-03 03:50:12 +0000

    doocrdir: optionally include colors levelst postprocessing on commandline

--- yb/doocrdir.sh
+++ yb/doocrdir.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
 CONVERT="convert"
 EXIFTOOL="exiftool"
-TESSERACT="tesseract -l eng+por --psm 1 --oem 1 -c tessedit_create_hocr=1"
+TESSERACT="tesseract -l eng+por --psm 1 --oem 1 -c tessedit_create_hocr=1 -c debug_file=/dev/null"
 HOCRTRANSFORM="hocrtransform.py"
-#SRCDIR="/data/FotoRestauro"
-SRCDIR="$1"
+TEMPDIR="${XDG_RUNTIME_DIR:-/tmp}"
+
+LEVEL="${2:-41%,76%}"
+SRCDIR=$(realpath --no-symlinks --quiet --canonicalize-existing "${1}") || { echo 1>&2 "$1 does not exist"; exit 1; } # "${1%%/}"
 BASESRCDIR="${SRCDIR##*/}"
-TEMPDIR="$XDG_RUNTIME_DIR"
-DESTDIR="$TEMPDIR"/"$BASESRCDIR"
+[ x"$BASESRCDIR"x == 'xx' ] && { echo 1>&2 'base directory name too short'; exit 1; }
+DESTDIR="$TEMPDIR"/"$$"-"$BASESRCDIR"
 mkdir "$DESTDIR"
-#read
 FILESJPG=()
 FILESHOCR=()
-inps=($(find "$SRCDIR" -name \*.tif | sort | tail --lines=+2 | head -n -1))
+inps=($(find "$SRCDIR" -iname \*.tif | sort ))
+#inps=("${inps[@]:1}") #skip first
+unset 'inps[${#inps[@]}-1]' #skip last
+unset 'inps[0]' #skip first
 for L in "${inps[@]}"
 do 
   BASENAME="${L##*/}"
@@ -19,20 +24,18 @@ do
   NAME="${BASENAME%.*}"
   DIR="${L%/*}"
   #DOSDIR="${DIR//\//\\}"
-  TEMPJPG="$TEMPDIR"/"$NAME".jpg
+  TEMPJPG="$TEMPDIR"/"$$"-"$NAME".jpg
   SRCTIFF="$L" # "$SRCDIR"/"$L"
   echo -n "$SRCTIFF -> $DESTDIR/$NAME.hocr; "
   $TESSERACT "$SRCTIFF" "$DESTDIR/$NAME"
   FILESHOCR+=("${DESTDIR}"/"${NAME}.hocr")
   echo -n "$SRCTIFF -> $TEMPJPG"
-  "$CONVERT" "$SRCTIFF" -level "27%,73%" -quality 86 -interlace Plane "$TEMPJPG"
+  "$CONVERT" "$SRCTIFF" -level "$LEVEL" -quality 86 -interlace Plane "$TEMPJPG"
   echo -n " -> $DESTDIR/$NAME.jpg" 
-  "$EXIFTOOL" -tagsFromFile "$SRCTIFF" -EXIF:ALL -XMP:ALL "$TEMPJPG" -PhotoshopThumbnail">"ThumbnailImage -o "$DESTDIR"/"$NAME.jpg"
+  "$EXIFTOOL" -tagsFromFile "$SRCTIFF" -EXIF:ALL -XMP:ALL "$TEMPJPG" -o "$DESTDIR"/"$NAME.jpg"
   touch -r "$SRCTIFF" -c "$DESTDIR"/"$NAME.jpg"
   FILESJPG+=("${DESTDIR}"/"${NAME}.jpg")
   rm "$TEMPJPG"
-  #exit
-  #echo copy /b \""$SRCDIRDOS"\\"$DOSPATH"\" _"$BASENAME"_ "$TEMPTIF"
 done
 python3 "$HOCRTRANSFORM" -f 'Helvetica' -o "$BASESRCDIR".pdf "${FILESHOCR[@]}"
 rm -v "${FILESJPG[@]}"
